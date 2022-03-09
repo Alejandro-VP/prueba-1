8afc236b54ad8b6c0069a1f10e4cc2a03b61d775
Raúl <raul.fernandez.cabanillas@gmail.com>
2022-03-09 12:59:53 +0100

    Nueva versión
    He añadido funcionalidad.
    
    Signed-off-by: Raúl <raul.fernandez.cabanillas@gmail.com>

--- AGit/src/ejemplo/HolaMundo.java
+++ AGit/src/ejemplo/HolaMundo.java
@@ -5,7 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola Mundo");
+		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("Qué tal");
 		
 	}
 
